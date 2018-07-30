cbaa80cbf93d8357fbb7cfd0dfb9d49ae6845d6c
Chandrahas Balleda <chandrahaswtw@gmail.com>
2018-07-30 12:39:53 +0530

    “WEB_CONSOLE_INTERFACE”

---
+++ PUBLIC/CLIENT_JS/index.js
@@ -0,0 +1,16 @@
+var socket = io();   //We are making a request from the client to the server -
+                        //to open up and web socket and keep that open.
+   
+   socket.on('connect',function (){
+     console.log('CONNECTED TO SERVER');
+   });
+   
+   socket.on('newMessage', function (message) {
+    console.log('newMessage', message);
+  });
+   
+   socket.on('disconnect',function () {
+     console.log('DISCONNECTED FROM SERVER');
+   });
+
+   
--- SERVER/server.js
+++ SERVER/server.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const hbs = require('hbs');
-const path = require('path')
+const path = require('path');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const app = express();
 const static_filepath = path.join(__dirname + './../PUBLIC');
 const port = process.env.PORT || '3000';
-
-
 app.set('view engine','hbs');
 app.use(express.static(__dirname + './../PUBLIC'));
 
-app.listen(port,()=>{console.log('THE APP STARTED ON PORT 3000')});
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection',(socket)=>{
+
+   console.log('NEW USER CONNECTED'); 
+
+   socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage',{
+      name : message.name,
+      text: message.text,
+     createdAt : new Date().getTime()
+    })
+});
+  
+   socket.on('disconnect',()=>{
+        console.log('USER DISCONNECTED');
+   }) 
+
+})
+
+server.listen(port,()=>{console.log(`THE APP STARTED ON PORT ${port}`)});
